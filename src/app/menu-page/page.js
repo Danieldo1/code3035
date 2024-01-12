c3625463744bc98fe127b77f4daa4f35c7e73e43
Dan <daniel.speranskiy@live.com>
2024-01-12 20:33:25 +0400

    Added smaller font for text in Bar Menu

--- src/app/menu-page/page.js
+++ src/app/menu-page/page.js
@@ -104,8 +104,8 @@ const MenuPageMain = () => {
             <div className=' flex-1 gap-5 justify-stretch w-full items-center'> 
       {categories.map(c => (
           <div id={c.name}  key={c._id} className='pt-10 mx-4'>
-         <button className='items-center justify-center text-center pb-10 w-full '>
-              <h2   className='text-center pb-3 font-bold text-3xl'>{c.name}</h2>
+         <button className='items-center justify-center text-center pb-5 w-full '>
+              <h2   className='text-center  font-bold text-2xl'>{c.name}</h2>
             </button>
           
                 <div className='flex flex-row flex-wrap flex-1 snap-mandatory snap-x  justify-stretch w-full '>
@@ -114,10 +114,10 @@ const MenuPageMain = () => {
                         <div key={item._id} className='flex snap-center justify-between w-full  items-center gap-2   bg-[#1B1918]'>
                             <div className='flex justify-between items-center gap-5'>
                                 <div className=''>
-                                    <h3 className='text-2xl font-bold'>{item.name}</h3>
-                                    <p className='text-lg text-gray-500'>{item.description}</p>
+                                    <h3 className='text-xl font-bold'>{item.name}</h3>
+                                    <p className='text-md text-gray-400'>{item.description}</p>
                                     { item.sizes.map((size) => (
-                                        <p key={size._id}> - {size.name}</p>
+                                        <p key={size._id} className='text-sm text-gray-400'> - {size.name}</p>
                                         ))}
                                     {item.extras.map((extra) => (
                                         <p className='text-sm  text-gray-500 capitalize' key={extra._id}>{extra.name}</p>
@@ -125,16 +125,16 @@ const MenuPageMain = () => {
                                 </div>
                             </div>
                             <div className='text-center'>
-                                <p className='text-2xl font-bold '>{item.price}</p>
+                                <p className='text-xl font-bold '>{item.price}</p>
                                     {item.sizes.map((size) => (
-                                        <p className='text-sm mt-10 text-gray-500 ' key={size._id}>{size.price + item.price}€</p>
+                                        <p className='text-sm mt-4 text-gray-500 ' key={size._id}>{size.price + item.price}€</p>
                                         ))}
                                         {item.extras.map((extra) => (
-                                            <p className='text-sm mt-1.5 text-gray-500 capitalize' key={extra._id}>+ {extra.price}</p>
+                                            <p className='text-sm mt-1 text-gray-500 capitalize' key={extra._id}>+ {extra.price}</p>
                                             ))}
                             </div>
                         </div>
-                                            <div className='border border-white   mx-auto  my-5 w-full' />
+                                            <div className='border border-white   mx-auto  my-2 w-full' />
                         </>
                     ))}
                 </div>    
