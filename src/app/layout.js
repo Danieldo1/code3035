2b535a4e075b9eb03b63d2559f1063c4b025e3fe
Dan <daniel.speranskiy@live.com>
2024-02-10 12:52:59 +0400

    Fiixing icons2

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,17 +21,17 @@ export default function RootLayout({ children }) {
       <head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <link
-  rel="icon"
-  href="/icon?<generated>"
-  type="image/<generated>"
-  sizes="<generated>"
-/>
-<link
-  rel="apple-touch-icon"
-  href="/apple-icon?<generated>"
-  type="image/<generated>"
-  sizes="<generated>"
-/>
+        rel="icon"
+        href="/icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
+      <link
+        rel="apple-touch-icon"
+        href="/apple-icon?<generated>"
+        type="image/<generated>"
+        sizes="<generated>"
+      />
       </head>
     <body className={`flex flex-col min-h-screen bg-[#1B1918] text-white ${rubik.className}`}>
       <AppProvider>
